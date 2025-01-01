18d2b9c86b471357ecae87c5901830f5f1cb570c
M.N.M <m.n.matusiak@gmail.com>
2025-01-01 15:56:32 +0100

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Marcin Matusiak on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code HistoryTests/Code_HistoryTests.swift
@@ -0,0 +1,16 @@
+//
+//  Code_HistoryTests.swift
+//  Code HistoryTests
+//
+//  Created by Marcin Matusiak on 1/1/25.
+//
+
+import Testing
+
+struct Code_HistoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
