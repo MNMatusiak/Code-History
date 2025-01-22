a261cf964d8677a6b5982bef27b252a4361e84a6
M.N.M <m.n.matusiak@gmail.com>
2025-01-22 23:12:20 +0100

    Question Model

---
+++ Code History/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Marcin Matusiak on 1/22/25.
+//
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
+
+
